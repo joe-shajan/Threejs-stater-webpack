7b00aed913bc3f6a3afd72a6dd600316618050b0
joeshajan <joeshajan1551@gmail.com>
2022-09-10 15:30:24 +0530

    first commit

---
+++ src/Experience/Experience.js
@@ -0,0 +1,51 @@
+import * as THREE from "three";
+import Sizes from "./Utils/Sizes.js";
+import Time from "./Utils/Time.js";
+import Camera from "./Camera.js";
+import Renderer from "./Renderer.js";
+import World from "./World/World";
+import Resources from "./Utils/Resources.js";
+import sources from "./sources.js";
+
+let instance = null;
+
+export default class Experience {
+  constructor(canvas) {
+    if (instance) return instance;
+    instance = this;
+    //global
+    window.Experience = this;
+
+    //options
+    this.canvas = canvas;
+
+    //setup
+    this.Sizes = new Sizes();
+    this.time = new Time();
+    this.scene = new THREE.Scene();
+    this.resources = new Resources(sources);
+    this.camera = new Camera();
+    this.renderer = new Renderer();
+    this.world = new World();
+
+    //sizes resize event
+    this.Sizes.on("resize", () => {
+      this.resize();
+    });
+
+    // time tick event
+    this.time.on("tick", () => {
+      this.update();
+    });
+  }
+
+  resize() {
+    this.camera.resize();
+    this.renderer.resize();
+  }
+
+  update() {
+    this.camera.update();
+    this.renderer.update();
+  }
+}
---
+++ src/Experience/World/World.js
@@ -0,0 +1,25 @@
+import * as THREE from "three";
+import Experience from "../Experience";
+import Environment from "./Environment";
+
+export default class World {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+    this.testMesh();
+
+    this.resources.on("ready", () => {
+      //setup
+      this.environment = new Environment();
+    });
+  }
+
+  testMesh() {
+    const mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshStandardMaterial()
+    );
+    this.scene.add(mesh);
+  }
+}
